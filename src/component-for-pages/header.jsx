3c9c54bb1d452a77f9a85b6e571cbc2c90dc9e79
ukeme-umana <ukeme_umana@yahoo.com>
2020-03-16 00:55:14 +0100

    Added bootstrap dependencies

--- src/component-for-pages/header.jsx
+++ src/component-for-pages/header.jsx
@@ -4,7 +4,43 @@ import {Link} from 'react-router-dom';
 
 function Header() {
     return (
-        <div class="header">
+        <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Page 1-1</a></li>
+            <li><a href="#">Page 1-2</a></li>
+            <li><a href="#">Page 1-3</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Page 2</a></li>
+        <li><a href="#">Page 3</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+        
+    );
+}
+export default Header;
+
+{/* <div class="header">
             <div class="header-logo-div">
                 <a href="https://thriveagric.com/">
                     <img src={thrivelogo} alt="thriveagric logo" class="header-logo"/>
@@ -51,7 +87,4 @@ function Header() {
                     </Link>
                 </ul>
             </div>
-        </div>
-    );
-}
-export default Header;
+        </div> */}
