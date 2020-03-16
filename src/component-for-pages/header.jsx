986824f64482d4de73ff5e58b602b6fea90053bb
ukeme-umana <ukeme_umana@yahoo.com>
2020-03-16 12:48:53 +0100

    changes

--- src/component-for-pages/header.jsx
+++ src/component-for-pages/header.jsx
@@ -12,10 +12,10 @@ function Header() {
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="#">WebSiteName</a>
+      <a class="navbar-brand" href="#"><img src={thrivelogo} alt="thriveagric logo" class="header-logo"/></a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
+      {/* <ul class="nav navbar-nav">
         <li class="active"><a href="#">Home</a></li>
         <li class="dropdown">
           <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
@@ -27,8 +27,14 @@ function Header() {
         </li>
         <li><a href="#">Page 2</a></li>
         <li><a href="#">Page 3</a></li>
-      </ul>
+      </ul> */}
       <ul class="nav navbar-nav navbar-right">
+        
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Home</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Our Process</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Our Farms</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> About Us</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Our Team</a></li>
         <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
         <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
       </ul>
