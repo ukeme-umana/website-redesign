c17f19a1eb21adf789efd3e2b1555e55a18e689d
ukeme-umana <ukeme_umana@yahoo.com>
2020-03-16 16:41:22 +0100

    aligned grey p text in stats section

--- src/component-for-pages/home/banner.jsx
+++ src/component-for-pages/home/banner.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 
 function Banner() {
     return (
-        <div class="body-banner-home">
-            <div class="body-banner-text2-home">
-                    <h2 className="banner-h2-text-home">Fund a Farmer <br/> Make a profit!</h2>
-                
-                    <p className="banner-p-text-home">
-                    Partner in our Journey to empower farmers and
-                    <br/>
-                     build an Africa that feeds the world and Itself.
-                    </p>
-            </div>
-            <div className="" >
-                <button className="button-to-fund-home">
-                    <a className="button-to-fund-link-home" href="blank">Fund a Farm</a>
-                </button>
+        <div className="col-md-12">
+            <div class="body-banner-home">
+                <div class="body-banner-text2-home">
+                        <h2 className="banner-h2-text-home">Fund a Farmer <br/> Make a profit!</h2>
+                    
+                        <p className="banner-p-text-home">
+                        Partner in our Journey to empower farmers and
+                        <br/>
+                         build an Africa that feeds the world and Itself.
+                        </p>
+                </div>
+                <div className="" >
+                    <button className="button-to-fund-home">
+                        <a className="button-to-fund-link-home" href="blank">Fund a Farm</a>
+                    </button>
+                </div>
             </div>
         </div>
     );
